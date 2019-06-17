bdc51ac28a4d2ba0144a9a1e23b76fdbc44784c4
XierHacker <xierhacker@163.com>
2019-06-17 11:58:47 +0800

    update

---
+++ GAN/DCGAN/DCGAN.py
@@ -0,0 +1,12 @@
+import numpy as np 
+import tensorflow as tf 
+
+
+class Generator(tf.keras.Model):
+    pass
+
+
+class Discriminator(tf.keras.Model):
+    pass
+
+
---
+++ GAN/DCGAN/train.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import tensorflow as tf
+
+
+result=tf.keras.datasets.mnist.load_data()
+print("result:\n",result)
